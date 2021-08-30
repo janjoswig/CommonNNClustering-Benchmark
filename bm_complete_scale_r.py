e11aa14a3f0d7de5a00448724ee5c015be8ad567
Jan-Oliver Joswig <jan.joswig@fu-berlin.de>
2021-08-30 14:46:16 +0200

    REFACTOR: bm notebook

---
+++ bm_complete_scale_r.py
@@ -0,0 +1,107 @@
+import json
+import pathlib
+
+from sklearn.metrics import pairwise_distances
+
+from cnnclustering import hooks
+
+import helper_base
+import cnnclustering_fit_cases as cases
+
+
+report_dir = pathlib.Path("reports/curta/cnnclustering_fit/complete")
+if not report_dir.is_dir():
+    report_dir.mkdir(parents=True, exist_ok=True)
+
+n_points_list = [500 * 2**x for x in range(10)]
+r_list = [0.2 * 0.95**x for x in range(10)]
+c_list = [int(2 * 2**x) for x in range(10)]
+
+raw_run_list = [
+    (
+        "varied_a_a_scale_r",
+        {
+            "r_list": r_list, "c_list": c_list, "d_list": 2,
+            "n_list": n_points_list,
+            "gen_func": helper_base.gen_blobs_points,
+            "gen_kwargs": {
+                "random_state": 170,
+                "cluster_std": [1.0, 2.5, 0.5]
+                },
+            "setup_kwargs": {
+                "recipe": cases.default_recipe
+                }
+            }
+        ),
+    (
+        "varied_b_a_scale_r",
+        {
+            "r_list": r_list, "c_list": c_list, "d_list": 2,
+            "n_list": n_points_list[:-2],
+            "gen_func": helper_base.gen_blobs_points,
+            "gen_kwargs": {
+                "random_state": 170,
+                "cluster_std": [1.0, 2.5, 0.5]
+                },
+            "setup_kwargs": {
+                "transform_func": pairwise_distances,
+                "recipe": cases.distance_recipe
+                }
+            }
+        ),
+    (
+        "varied_d_a_scale_r",
+        {
+            "r_list": r_list, "c_list": c_list, "d_list": 2,
+            "n_list": n_points_list[:-1],
+            "gen_func": helper_base.gen_blobs_points,
+            "gen_kwargs": {
+                "random_state": 170,
+                "cluster_std": [1.0, 2.5, 0.5]
+                },
+            "setup_kwargs": {
+                "transform_func": helper_base.compute_neighbours,
+                "transform_args": ("<r>",),
+                "transform_kwargs": {"sort": True},
+                "preparation_hook": hooks.prepare_neighbourhoods,
+                "recipe": cases.neighbours_sorted_recipe
+                }
+            }
+        ),
+    # (
+    #     "varied_c_a_scale_r",
+    #     {
+    #         "r_list": r_list, "c_list": c_list, "d_list": 2,
+    #         "n_list": n_points_list[:-1],
+    #         "gen_func": helper_base.gen_blobs_points,
+    #         "gen_kwargs": {
+    #             "random_state": 170,
+    #             "cluster_std": [1.0, 2.5, 0.5]
+    #             },
+    #         "setup_kwargs": {
+    #             "transform_func": helper_base.compute_neighbours,
+    #             "transform_args": ("<r>",),
+    #             "preparation_hook": hooks.prepare_neighbourhoods,
+    #             "recipe": cases.neighbours_recipe
+    #             }
+    #         }
+    #     )
+    ]
+
+run_list = (
+    helper_base.Run(
+        run_name,
+        cases.gen_bm_units_cnnclustering_complete(**kwargs),
+    )
+    for run_name, kwargs in raw_run_list
+)
+
+if __name__ == "__main__":
+    for run in run_list:
+
+        report_file = report_dir / f"{run.run_name}_raw.json"
+
+        run.collect(v=True)
+
+        with open(report_file, "w") as fp:
+            json.dump(run.timings, fp, indent=4)
